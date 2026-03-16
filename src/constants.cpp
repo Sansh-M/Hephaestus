b68c5a1d9b8090421784a55d7fa44915067bdd5c
sansh <sansh@SANSHRAY_S_PC>
2026-03-16 15:38:23 +0100

    Add project files.

---
+++ src/constants.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+// constants
+const float GRAVITY = 9.81f; // m/s^2
+const float GRAV_CONST = 6.67430e-11f; // m^3 kg^-1 s^-2
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Physics Engine starting...\n";
+    return 0;
+}
