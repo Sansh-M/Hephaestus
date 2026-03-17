79b55680052f9581194f31a42551c03bf39620fd
sansh <sansh@SANSHRAY_S_PC>
2026-03-17 15:43:31 +0100

    Changes to main and universalfunctions

--- src/UniversalFunctions.hpp
+++ src/UniversalFunctions.hpp
@@ -1,7 +1,25 @@
 #include <iostream>
 #include <vector>
 #pragma once
-
+#include "PlanetaryBodies.hpp"
+/*
+set of functions related to gravitational interaction between entities
+*/
 namespace Gravity_Components {
 
+}
+
+/*
+functions that are used for building components and the environment 
+*/
+namespace Build_Functions {
+	
+	void buildEntity() {
+
+	}
+
+	PlanetaryBodies buildPlanetaryBodies(Vec3 origin, float mass, float radius, float atmospheric_density) {
+		PlanetaryBodies p = PlanetaryBodies(origin, mass, radius, atmospheric_density);
+		return p;
+	}
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
-#include "raylib.h"
+#include "UniversalFunctions.hpp"
 
 int main() {
     std::cout << "Physics Engine starting...\n";
     bool EXIT = false;
-
+    //main engine loop 
     while (EXIT = false) {
+        //TODO: create a linked list potentially in order to store the planetary bodies relative to each other. 
+
+
 
     }
-
     return 0;
 }
