750e56b7ddf8d0030665d1ec2ad3b21e0c79c426
sansh <sansh@SANSH_LAPTOP>
2026-07-14 00:45:38 +0200

    Expanded planetary body to contain structs which are needed for the orbital motion of planets following keplerian motion. TODO: add function to update the transformation matrix and then a functino to calculate the updated position vector for the planet and then set the value in the planet object. c++ multithreading will be used to do this.

--- src/PlanetaryBody.hpp
+++ src/PlanetaryBody.hpp
@@ -3,13 +3,37 @@
 #include "constants.hpp"
 #pragma once
 
+struct OrbitParams {
+	float semiMajorAxis;
+	float eccentricity;
+	float orbitalPeriod;      // seconds
+	float meanAnomaly0;       // radians, at t = 0
+	float inclination;        // radians, tilt of orbital plane
+	float raan;               // radians, longitude of ascending node (Ω)
+	float argOfPeriapsis;     // radians, orientation of ellipse within its plane (ω)
+};
+
+/*
+Struct to define the transition matrix for the orbital rotation per timestep.
+*/
+struct RotationFrame {
+	float R11, R12, R21, R22, R31, R32; // Rotation matrix components
+};
+
+/*
+Struct to define the parameters for the rotation of the orbit
+*/
+struct RotationState {
+	float angle0; // Initial angle of rotation (radians)
+	float angularVelocity; // Angular velocity of rotation (radians per second)
+};
 
 /*
 Class used to define planetary bodies.
 */
 class PlanetaryBody {
 public:
-	PlanetaryBody(std::string name, const Vec3& origin, Vec3& pos, float mass, float radius, std::vector<std::string> CSVPATHS);
+	PlanetaryBody(std::string name, const Vec3& origin, Vec3& pos, float mass, float radius, std::vector<std::string> CSVPATHS, OrbitParams orbit_params, RotationFrame rotation_frame, RotationState rotation_state);
 
 	/*
 	set the position of the planetary body after it has been computed for the next frame. 
@@ -25,4 +49,8 @@ private:
 	float mass;
 	float radius;
 	std::vector<std::string> CSVPATHS;
+	OrbitParams orbit_params;
+	RotationFrame rotation_frame;
+	RotationState rotation_state;
+
 };
