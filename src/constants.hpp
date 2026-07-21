0339766acebd53ce76f6bad271635c8345828da1
Sansh-M <sanshray.malhotra@gmail.com>
2026-07-21 04:56:52 +0200

    Fixes made to the vector normalize function in constants.hpp which was blocking compilation.

--- src/constants.hpp
+++ src/constants.hpp
@@ -42,9 +42,23 @@ struct Vec3 { float x,y,z;
 	}
 
 	//function to normalize a vector 
-	Vec3 normalize(Vec3 point, Vec3 vec) {
-		float vecLength = sqrt((vec.x - point.x) ^ 2.0 + (vec.y - point.y) ^ 2.0 + (vec.z - point.z) ^ 2.0);
-		return Vec3{ (vec.x / vecLength), (vec.y / vecLength), (vec.z / vecLength) };
+	Vec3 normalize(Vec3 vec)
+	{
+		float vecLength = std::sqrt(
+			vec.x * vec.x +
+			vec.y * vec.y +
+			vec.z * vec.z
+		);
+
+		if (vecLength == 0.0f) {
+			return { 0.0f, 0.0f, 0.0f };
+		}
+
+		return {
+			vec.x / vecLength,
+			vec.y / vecLength,
+			vec.z / vecLength
+		};
 	}
 };
 
