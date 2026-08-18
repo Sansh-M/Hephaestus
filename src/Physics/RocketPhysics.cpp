a07a583d7452fde60a95e11e7875d10b42aa9f0e
Sansh-M <sanshray.malhotra@gmail.com>
2026-08-18 17:53:23 +0200

    Added file in order to update the acceleration vector fo a rocket given user input and thrust from rocket. Contains multiple functions to update the acceleration vector from both engines, rcs, flaps and atmospheric affects. Uses the update function in Universe.cpp

---
+++ src/Physics/RocketPhysics.cpp
@@ -0,0 +1,21 @@
+#include "Entities.hpp"
+#include <iostream>
+#include "PlanetaryBody.hpp"
+#include "Universe.hpp"
+#include "constants.hpp"
+
+/*
+Class with functions that define the motion of rockets defined by user inputs.
+*/
+class RocketPhysics {
+
+	/*
+	Function to define the change in motion given thrust vector from rocket engine.
+	Returns the modified acceleration vector which can then be used to update the velocity and position of the entity using Universe.advance()	
+	*/
+	
+	Vec3 ThrustAcceleration(const Vec3& planetary_acceleration, Entity entity, Vec3 thrust_vector) {
+
+	}
+
+};
